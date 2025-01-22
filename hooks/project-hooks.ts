3734c87f999fa563113939c40d914732f27afe84
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2025-01-22 12:22:53 +0000

    hooks for the crud of projects completed

---
+++ hooks/project-hooks.ts
@@ -0,0 +1,151 @@
+import { db } from "@/lib/db"
+import { Project } from "@/types"
+import { CheckUser } from "@/utils"
+
+
+
+export async function GetProjects(){
+    try {
+        const user = await CheckUser()
+        if(!user){
+            return null
+        }
+
+        const userProjects = await db.project.findMany({
+            where:{
+                userId:user.id
+            }
+        })
+
+        return userProjects
+        
+    } catch (error:any) {
+        console.log("error", error.message)
+        return null
+        
+    }
+}
+
+export async function GetAProject(projectId:string){
+    try {
+        const user = await CheckUser()
+        if(!user){
+            return null
+        }
+
+
+        const project = await db.project.findFirst({
+            where:{
+                id:projectId,
+                userId:user.id
+            }
+        })
+        
+        return project
+
+        
+    } catch (error:any) {
+        console.log("error", error.message)
+        return null
+        
+    }
+}
+
+export async function CreateProject(data:Project){
+    try {
+
+        const user = await CheckUser()
+        if(!user){
+            return null
+        }
+
+       const project =  await db.project.create({
+            data:{
+                userId:user.id,
+                ...data
+            }
+        })
+
+        return project
+        
+    } catch (error:any) {
+        console.log("error", error.message)
+        return null
+        
+    }
+}
+
+export async function UpdateProject(projectId:string, data:Project){
+    try {
+
+
+        const user = await CheckUser()
+
+        if(!user){
+            return null
+        }
+
+        const checkProject = await db.project.findFirst({
+            where:{
+                id:projectId,
+                userId:user.id
+            }
+        })
+
+        if(!checkProject){
+            return null
+        }
+
+        const updatedProject = await db.project.update({
+            where:{
+                id:projectId,
+                userId:user.id
+            },
+            data:{
+                ...data
+            }
+        })
+
+        
+    return updatedProject
+
+        
+    } catch (error:any) {
+        console.log("error", error.message)
+        return null
+        
+    }
+}
+
+export async function DeleteProject(projectId:string){
+    try {
+        const user = await CheckUser()
+        if(!user){
+            return null
+        }
+
+        const checkProject = await db.project.findFirst({
+            where:{
+                id:projectId,
+                userId:user.id
+            }
+        })
+
+        if(!checkProject){
+            return null
+        }
+
+        await db.project.delete({
+            where:{
+                id:projectId
+            }
+        })
+
+        return "Project deleted"
+        
+    } catch (error:any) {
+        console.log("error", error.message)
+        return null
+        
+    }
+}
